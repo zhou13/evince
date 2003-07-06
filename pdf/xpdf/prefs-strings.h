95e963b4cfd12e7ad2c7bbdcee74bf3e45a766a2
Remi Cohen-Scali <rcoscali@cvs.gnome.org>
2003-07-06 12:28:00 +0000

    Preferences gconf ids for storing sidebar state.
    2003-07-06  Remi Cohen-Scali  <rcoscali@cvs.gnome.org>
    
    	* xpdf/prefs-strings.h (CONF_WINDOWS_SIDEBAR_SIZE)
    	(CONF_WINDOWS_SIDEBAR_PAGE, CONF_WINDOWS_SHOW_SIDEBAR):
    	Preferences gconf ids for storing sidebar state.

---
+++ pdf/xpdf/prefs-strings.h
@@ -0,0 +1,4 @@
+/* Window appeareance */
+#define CONF_WINDOWS_SIDEBAR_PAGE "/apps/galeon/UI/Windows/sidebar_page"
+#define CONF_WINDOWS_SIDEBAR_SIZE "/apps/galeon/UI/Windows/sidebar_size"
+#define CONF_WINDOWS_SHOW_SIDEBAR "/apps/galeon/UI/Windows/show_sidebar"
