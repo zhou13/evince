bd82fd8cb01e25aaf3d634406532879d90dec7d1
Remi Cohen-Scali <rcoscali@cvs.gnome.org>
2003-07-13 05:31:38 +0000

    Changed galeon with gpdf (copy/paste typo).
    2003-07-13  Remi Cohen-Scali  <rcoscali@cvs.gnome.org>
    
    	* xpdf/prefs-strings.h (CONF_WINDOWS_SIDEBAR_PAGE)
    	(CONF_WINDOWS_SIDEBAR_SIZE, CONF_WINDOWS_SHOW_SIDEBAR):
    	Changed galeon with gpdf (copy/paste typo).

--- pdf/xpdf/prefs-strings.h
+++ pdf/xpdf/prefs-strings.h
@@ -1,4 +1,4 @@
 /* Window appeareance */
-#define CONF_WINDOWS_SIDEBAR_PAGE "/apps/galeon/UI/Windows/sidebar_page"
-#define CONF_WINDOWS_SIDEBAR_SIZE "/apps/galeon/UI/Windows/sidebar_size"
-#define CONF_WINDOWS_SHOW_SIDEBAR "/apps/galeon/UI/Windows/show_sidebar"
+#define CONF_WINDOWS_SIDEBAR_PAGE "/apps/gpdf/UI/Windows/sidebar_page"
+#define CONF_WINDOWS_SIDEBAR_SIZE "/apps/gpdf/UI/Windows/sidebar_size"
+#define CONF_WINDOWS_SHOW_SIDEBAR "/apps/gpdf/UI/Windows/show_sidebar"
