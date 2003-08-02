d597ef73ef3895ed894992bd612d74d0264c8c12
Remi Cohen-Scali <rcoscali@cvs.gnome.org>
2003-08-02 02:16:59 +0000

    Added 'annot is displayed' callback as params when displaying pages.
    2003-08-02  Remi Cohen-Scali  <rcoscali@cvs.gnome.org>
    
    	* xpdf/Page.h, xpdf/Page.cc: Added 'annot is
    	displayed' callback as params when displaying pages.
    	(Page::displaySlice): Actually draw annot if
    	callback: tells we should.

--- pdf/xpdf/Page.h
+++ pdf/xpdf/Page.h
@@ -22,6 +22,8 @@ class XRef;
 class OutputDev;
 class Links;
 class Catalog;
+class Annots;
+class Annot;
 
 //------------------------------------------------------------------------
 
@@ -147,14 +149,18 @@ public:
   void display(OutputDev *out, double dpi, int rotate,
 	       Links *links, Catalog *catalog,
 	       GBool (*abortCheckCbk)(void *data) = NULL,
-	       void *abortCheckCbkData = NULL);
+	       void *abortCheckCbkData = NULL,
+               GBool (*annotDisplayDecideCbk)(Annot *annot, void *user_data) = NULL,
+               void *annotDisplayDecideCbkData = NULL);
 
   // Display part of a page.
   void displaySlice(OutputDev *out, double dpi, int rotate,
 		    int sliceX, int sliceY, int sliceW, int sliceH,
 		    Links *links, Catalog *catalog,
 		    GBool (*abortCheckCbk)(void *data) = NULL,
-		    void *abortCheckCbkData = NULL);
+		    void *abortCheckCbkData = NULL,
+                    GBool (*annotDisplayDecideCbk)(Annot *annot, void *user_data) = NULL,
+                    void *annotDisplayDecideCbkData = NULL);
 
 private:
 
